f513f166724ae1cd4017871b3412843f75ffbc15
halim_ak <aktashalim.pro@gmail.com>
2024-05-06 05:17:48 +0300

    fix bug theme mode when charging page

--- client/src/Utils/ParsingData.tsx
+++ client/src/Utils/ParsingData.tsx
@@ -47,9 +47,9 @@ export function formatLikeCount(views: number) {
 export function changeDarkMode() {
   const theme = localStorage.getItem("theme");
   if (theme === "dark") {
-    document.documentElement.classList.add("dark");
-  } else {
     document.documentElement.classList.remove("dark");
+  } else {
+    document.documentElement.classList.add("dark");
   }
 
   localStorage.setItem("theme", theme === "dark" ? "light" : "dark");
