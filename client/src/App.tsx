cd38d31db1d5444294cd6a7eca8e2b6284eaf7af
Halim78 <halim@mkdmpartner.com>
2024-05-07 00:31:19 +0200

    adding analytics package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import HomeNew from "./components/HomeNew";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
       <HomeNew />
+      <Analytics />
     </div>
   );
 }
